25bb215bb7377bf9b4e5310297edea91bdf1fda9
kibum0405 <59447401+kibum0405@users.noreply.github.com>
2021-07-08 14:57:09 +0900

    Add files via upload

--- src/.temp/config.js
+++ src/.temp/config.js
@@ -1,7 +1,7 @@
 export default {
   "trailingSlash": true,
   "pathPrefix": "",
-  "titleTemplate": "%s - intro-msae",
+  "titleTemplate": "%s",
   "siteUrl": "https://example.com",
   "version": "0.7.22",
   "catchLinks": true
