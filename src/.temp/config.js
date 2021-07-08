013bbe9d8040d600be101a62d08e52b0db438e0f
jinyoung jang <jyjang@uengine.org>
2021-07-08 10:45:18 +0900

    change

---
+++ src/.temp/config.js
@@ -0,0 +1,8 @@
+export default {
+  "trailingSlash": true,
+  "pathPrefix": "",
+  "titleTemplate": "%s - intro-msae",
+  "siteUrl": "https://example.com",
+  "version": "0.7.22",
+  "catchLinks": true
+}
---
+++ src/.temp/routes.js
@@ -0,0 +1,45 @@
+const c1 = () => import(/* webpackChunkName: "page--src--templates--markdown-page-vue" */ "/Users/jangjinyoung/msa-ez.github.io/src/templates/MarkdownPage.vue")
+const c2 = () => import(/* webpackChunkName: "page--src--pages--404-vue" */ "/Users/jangjinyoung/msa-ez.github.io/src/pages/404.vue")
+const c3 = () => import(/* webpackChunkName: "page--src--pages--index-vue" */ "/Users/jangjinyoung/msa-ez.github.io/src/pages/Index.vue")
+
+export default [
+  {
+    path: "/started/writing-content/",
+    component: c1
+  },
+  {
+    path: "/started/settings/",
+    component: c1
+  },
+  {
+    path: "/started/sidebar/",
+    component: c1
+  },
+  {
+    path: "/started/installation/",
+    component: c1
+  },
+  {
+    path: "/started/deploying/",
+    component: c1
+  },
+  {
+    path: "/started/",
+    component: c1
+  },
+  {
+    name: "404",
+    path: "/404/",
+    component: c2
+  },
+  {
+    name: "home",
+    path: "/",
+    component: c3
+  },
+  {
+    name: "*",
+    path: "*",
+    component: c2
+  }
+]
