535f7a1e85b7559823611df19f42ab9f7b349de5
kibumpark <top5w5@naver.com>
2024-01-05 17:06:19 +0900

    gitignore

--- src/.temp/icons.js
+++ src/.temp/icons.js
@@ -5,37 +5,37 @@ export default {
   "touchicons": [
     {
       "width": 76,
-      "src": "/assets/static/src/assets/favicon.png?width=76&key=7c9b520"
+      "src": "/assets/static/src/img/logo.png?width=76&key=46987f1"
     },
     {
       "width": 152,
-      "src": "/assets/static/src/assets/favicon.png?width=152&key=7c9b520"
+      "src": "/assets/static/src/img/logo.png?width=152&key=46987f1"
     },
     {
       "width": 120,
-      "src": "/assets/static/src/assets/favicon.png?width=120&key=7c9b520"
+      "src": "/assets/static/src/img/logo.png?width=120&key=46987f1"
     },
     {
       "width": 167,
-      "src": "/assets/static/src/assets/favicon.png?width=167&key=7c9b520"
+      "src": "/assets/static/src/img/logo.png?width=167&key=46987f1"
     },
     {
       "width": 180,
-      "src": "/assets/static/src/assets/favicon.png?width=180&key=7c9b520"
+      "src": "/assets/static/src/img/logo.png?width=180&key=46987f1"
     }
   ],
   "favicons": [
     {
       "width": 16,
-      "src": "/assets/static/src/assets/favicon.png?width=16&key=5fc512c"
+      "src": "/assets/static/src/img/logo.png?width=16&key=5e8f2fa"
     },
     {
       "width": 32,
-      "src": "/assets/static/src/assets/favicon.png?width=32&key=5fc512c"
+      "src": "/assets/static/src/img/logo.png?width=32&key=5e8f2fa"
     },
     {
       "width": 96,
-      "src": "/assets/static/src/assets/favicon.png?width=96&key=5fc512c"
+      "src": "/assets/static/src/img/logo.png?width=96&key=5e8f2fa"
     }
   ]
 }
