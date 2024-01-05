92fda5c5ce56ef024ffea9269b234fb53af7a37d
kibumpark <top5w5@naver.com>
2024-01-05 18:34:44 +0900

    Fixed untracked files

---
+++ src/.temp/icons.js
@@ -0,0 +1,41 @@
+export default {
+  "touchiconMimeType": "image/png",
+  "faviconMimeType": "image/png",
+  "precomposed": false,
+  "touchicons": [
+    {
+      "width": 76,
+      "src": "/assets/static/logo.f22e9f3.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 152,
+      "src": "/assets/static/logo.62d22cb.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 120,
+      "src": "/assets/static/logo.1539b60.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 167,
+      "src": "/assets/static/logo.dc0cdc5.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 180,
+      "src": "/assets/static/logo.7b22250.bd727e13501e82a0ba322b4ad29fd33a.png"
+    }
+  ],
+  "favicons": [
+    {
+      "width": 16,
+      "src": "/assets/static/logo.ce0531f.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 32,
+      "src": "/assets/static/logo.ac8d93a.bd727e13501e82a0ba322b4ad29fd33a.png"
+    },
+    {
+      "width": 96,
+      "src": "/assets/static/logo.b9532cc.bd727e13501e82a0ba322b4ad29fd33a.png"
+    }
+  ]
+}
