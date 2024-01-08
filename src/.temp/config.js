9f635b6823e0e66254538ecbdeb8d4cd0389b3a2
kibumpark <top5w5@naver.com>
2024-01-08 09:59:13 +0900

    Fixed untracked files

--- src/.temp/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  "trailingSlash": true,
-  "pathPrefix": "",
-  "titleTemplate": "%s - msaez",
-  "siteUrl": "https://example.com",
-  "version": "0.7.23",
-  "catchLinks": true
-}
