d0a54afa369ba3a8fb78337a42544da1e1634709
kibumpark <top5w5@naver.com>
2024-01-09 14:05:38 +0900

    페이지 간격 조절

--- src/templates/MarkdownPage.vue
+++ src/templates/MarkdownPage.vue
@@ -1,10 +1,10 @@
 <template>
   <Layout>
     <div class="flex flex-wrap items-start justify-start">
-      <div class="order-2 w-full md:w-1/6 sm:pl-4 md:pl-6 lg:pl-8 sticky" style="top: 4rem">
+      <div class="order-2 w-full md:w-1/4 sm:pl-4 md:pl-6 lg:pl-8 sticky" style="top: 4rem">
         <OnThisPage />
       </div>
-      <div class="order-1 w-full md:w-5/6">
+      <div class="order-1 w-full md:w-3/4">
         <div class="content" v-html="$page.markdownPage.content" />
         <div class="mt-8 pt-8 lg:mt-12 lg:pt-12 border-t border-ui-border">
           <NextPrevLinks />
